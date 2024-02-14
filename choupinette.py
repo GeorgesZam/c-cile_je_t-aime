50fc03e210ad671d98c3f7b6980bef88bf51c0f0
GeorgesZam <154804622+GeorgesZam@users.noreply.github.com>
2024-02-14 21:43:26 +0100

    Update choupinette.py

--- choupinette.py
+++ choupinette.py
@@ -5,14 +5,9 @@ from PIL import Image
 st.title("Joyeuse Saint-Valentin !")
 
 # Message personnalisé
-message = "Mon amour, je te souhaite une merveilleuse Saint-Valentin. Voici quelques souvenirs de nos moments ensemble. Je t'aime !"
+message = "Ma choupinette, je te souhaite une merveilleuse Saint-Valentin. Voici quelques souvenirs de nos moments ensemble. Je t'aime !"
 st.write(message)
-"""
-# Afficher une image
-image_path = "chemin_vers_ta_photo.jpg"
-image = Image.open(image_path)
-st.image(image, caption="Nous ❤️")
-"""
+
 # Jouer une chanson
 st.write("Notre chanson préférée en vidéo :")
 url_video = "https://www.youtube.com/watch?v=v8oqbWrP1QY"
