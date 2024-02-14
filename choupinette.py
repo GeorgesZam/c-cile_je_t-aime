34e480276044aac3c5c742898c5b338469e8bdfa
GeorgesZam <154804622+GeorgesZam@users.noreply.github.com>
2024-02-14 21:48:20 +0100

    Update choupinette.py

--- choupinette.py
+++ choupinette.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from PIL import Image
+import datetime
+import time
 
 # Titre
 st.title("Joyeuse Saint-Valentin !")
@@ -9,6 +11,34 @@ message = "Ma choupinette, je te souhaite une merveilleuse Saint-Valentin. Voici
 st.write(message)
 
 # Jouer une chanson
+
 st.write("Notre chanson préférée en vidéo :")
 url_video = "https://www.youtube.com/watch?v=v8oqbWrP1QY"
 st.video(url_video)
+
+#compteur
+
+# Définir la date de début de la relation
+start_date = datetime.datetime(2023, 10, 1)
+
+# Calculer la différence de temps
+def calculate_time_difference(start_date):
+    now = datetime.datetime.now()
+    time_difference = now - start_date
+    return time_difference
+
+# Afficher le compteur
+def display_counter():
+    st.write("Depuis que nous sommes ensemble :")
+    time_difference = calculate_time_difference(start_date)
+    st.write(f"{time_difference.days} jours, {time_difference.seconds // 3600} heures, "
+             f"{(time_difference.seconds // 60) % 60} minutes et {time_difference.seconds % 60} secondes.")
+
+display_counter()
+
+# Mettre à jour le compteur toutes les secondes
+while True:
+    display_counter()
+    time.sleep(1)
+    st.experimental_rerun()
+
